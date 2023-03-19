b759da01b22e93ec485e93ae5539cbbcba8b9299
Alexander Korsunsky <A.Korsunsky@gmail.com>
2023-03-19 02:32:22 +0100

    Add MetricsWriter class

--- dogfinder_train.py
+++ dogfinder_train.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import math
-from typing import Iterable, List, Optional, Tuple
+import re
+from typing import Dict, Iterable, List, Optional, Tuple
 
 import numpy as np
 import matplotlib
@@ -53,19 +53,44 @@ CLASS_MAX = np.amax([c for c in classnames.keys()])
 CLASS_DOG = 12
 
 
-def dog_only(target):
-    dogness = torch.where(target == CLASS_DOG, torch.ones_like(target, dtype=torch.int8), torch.zeros_like(target, dtype=torch.int8))
-    return dogness
+class MetricsWriter():
+    def __init__(self, tb_writer: SummaryWriter, max_len: int, scalar_tags: Optional[List[str]]=None):
+        self.__tb_writer: SummaryWriter = tb_writer
+        self.__max_len: int = max_len
 
+        if scalar_tags is None:
+            scalar_tags = list()
 
-def find_nonzero_masks(ds_iter: Iterable[Tuple[torch.Tensor, torch.Tensor]]) -> List:
-    nonzero_masks = list()
-    for idx, sample in enumerate(ds_iter):
-        if sample[1].count_nonzero().item() > 0:
-            nonzero_masks.append(idx)
+        self.__scalar_metrics: Dict[str, np.array] = dict()
+
+        for scalar in scalar_tags:
+            self.add_scalar(scalar)
     
-    return nonzero_masks
+        self.__current_step: int = 0
+    
+    def add_scalar(self, tag: str):
+        assert tag not in self.__scalar_metrics.keys(), "Tag has already been added"
+        self.__scalar_metrics[tag] = np.empty((self.__max_len,))
+    
+    def set_step(self, step: int):
+        self.__current_step = step
 
+    def add_sample(self, tag: str, value: float):
+        self.__scalar_metrics[tag][self.__current_step] = value
+    
+    def get(self, tag: str):
+        return np.nanmean(self.__scalar_metrics[tag])
+
+    def write(self, global_step: int):
+        for tag in self.__scalar_metrics.keys():
+            metric_value = self.get(tag)
+            self.__tb_writer.add_scalar(tag, metric_value, global_step=global_step)
+
+    def clear(self):
+        self.__current_step = 0
+        for samples in self.__scalar_metrics.values():
+            samples.fill(np.nan)
+        
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 nproc=8
@@ -122,16 +147,19 @@ def train(
     writer.add_graph(model, ds_train[0][0].unsqueeze(0).to(device))
 
     # initialize epoch metrics
-    epoch_metrics = {m : np.empty((len(train_dataloader),)) for m in [
+    metrics_train_epoch = MetricsWriter(writer, max_len=len(train_dataloader), scalar_tags=[
         "Loss/train/total",
         "Loss/train/pixelclass",
         "Loss/train/boundary",
-        "Accuracy/train/pixelwise",
+        "Accuracy/train/pixelwise"
+    ])
+
+    metrics_val_epoch = MetricsWriter(writer, max_len=len(val_dataloader), scalar_tags=[
         "Loss/val/total",
         "Loss/val/pixelclass",
         "Loss/val/boundary",
         "Accuracy/val/pixelwise",
-    ]}
+    ])
 
     ds_train_len = len(ds_train)
     global_step = lambda: (
@@ -141,6 +169,8 @@ def train(
     for epoch in tqdm(range(num_epochs), desc="Epochs", unit="epochs"):
         for batch_idx, batch in enumerate(tqdm(train_dataloader, desc="Batches (train)", unit="batch")):
             img, mask = batch
+
+            metrics_train_epoch.set_step(batch_idx)
 
             # bring sample to device
             img: torch.Tensor = img.to(device=device)
@@ -175,9 +205,9 @@ def train(
 
             loss = (1.-boundary_loss_weight) * loss_pixelclass + boundary_loss_weight * loss_boundary
 
-            epoch_metrics['Loss/train/total'][batch_idx] = loss.item()
-            epoch_metrics['Loss/train/pixelclass'][batch_idx] = loss_pixelclass.item()
-            epoch_metrics['Loss/train/boundary'][batch_idx] = loss_boundary.item()
+            metrics_train_epoch.add_sample('Loss/train/total', loss.item())
+            metrics_train_epoch.add_sample('Loss/train/pixelclass', loss_pixelclass.item())
+            metrics_train_epoch.add_sample('Loss/train/boundary', loss_boundary.item())
             
             optimizer.zero_grad()
             loss.backward()
@@ -187,17 +217,12 @@ def train(
                 # calculate pixel-wise annoation accuracy for all imgs in batch
                 acc = (mask == torch.argmax(pred, dim=1))
                 acc = (acc.sum()/acc.numel())
-                epoch_metrics['Accuracy/train/pixelwise'][batch_idx] = acc.item()
+                metrics_train_epoch.add_sample('Accuracy/train/pixelwise', acc.item())
 
-        for metric_name, metric_values in epoch_metrics.items():
-            if "/train/" not in metric_name:
-                continue # only writing training metrics at this point
+        metrics_train_epoch.write(global_step=global_step())
 
-            metric_value = np.nanmean(metric_values)
-            writer.add_scalar(metric_name, metric_value, global_step=global_step())
-
-        tqdm.write(f"Epoch {epoch+1}; Loss/train/total={np.nanmean(epoch_metrics['Loss/train/total']):.4f};" +
-            f"Accuracy/train/pixelwise={np.nanmean(epoch_metrics['Accuracy/train/pixelwise']):.3f}")
+        tqdm.write(f"Epoch {epoch+1}; Training Loss: {metrics_train_epoch.get('Loss/train/total'):.4f}; " +
+            f"Training Pixel Accuracy: {metrics_train_epoch.get('Accuracy/train/pixelwise'):.3f}")
 
         if epoch % val_epoch_freq == val_epoch_freq - 1:
             with torch.no_grad():
@@ -218,21 +243,18 @@ def train(
 
                     loss = (1.-boundary_loss_weight) * loss_pixelclass + boundary_loss_weight * loss_boundary
 
-                    epoch_metrics['Loss/val/total'][val_batch_idx] = loss.item()
-                    epoch_metrics['Loss/val/pixelclass'][val_batch_idx] = loss_pixelclass.item()
-                    epoch_metrics['Loss/val/boundary'][val_batch_idx] = loss_boundary.item()
+                    metrics_val_epoch.set_step(val_batch_idx)
+                    metrics_val_epoch.add_sample('Loss/val/total', loss.item())
+                    metrics_val_epoch.add_sample('Loss/val/pixelclass', loss_pixelclass.item())
+                    metrics_val_epoch.add_sample('Loss/val/boundary', loss_boundary.item())
 
                     # calculate pixel-wise annoation accuracy for alyl imgs in batch
                     acc = (mask == torch.argmax(pred, dim=1))
                     acc = (acc.sum()/acc.numel())
-                    epoch_metrics['Accuracy/val/pixelwise'][val_batch_idx] = acc.item()
+                    metrics_val_epoch.add_sample('Accuracy/val/pixelwise', acc.item())
 
-            for metric_name, metric_values in epoch_metrics.items():
-                if "/val/" not in metric_name:
-                    continue # only writing validation metrics at this point
 
-                metric_value = np.nanmean(metric_values)
-                writer.add_scalar(metric_name, metric_value, global_step=global_step())
+            metrics_val_epoch.write(global_step=global_step())
 
             # prepare comparison grid for the first three samples in dataset
             vis_samples = 3 # number of samples to visualize per image
@@ -244,8 +266,8 @@ def train(
             comparison_fig_t = visualize.make_comparison_grid(inv_normalize(val_imgs), pred_amax, val_masks)
             writer.add_image("PredictionComparison", comparison_fig_t, global_step=global_step())
 
-            tqdm.write(f"Epoch {epoch+1}; 'Loss/val/total'={np.nanmean(epoch_metrics['Loss/val/total']):.4f}; " +
-                f"Accuracy/val/pixelwise={np.nanmean(epoch_metrics['Accuracy/val/pixelwise']):.3f}")
+            tqdm.write(f"Epoch {epoch+1}; Validation Loss: {metrics_val_epoch.get('Loss/val/total'):.4f}; " +
+                f"Validation Pixel Accuracy: {metrics_val_epoch.get('Accuracy/val/pixelwise'):.3f}")
 
     # writer.add_hparams(
     #     hparam_dict={
