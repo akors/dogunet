bb6f067b844b9d4bcb30b787b46c4ca1c6c8c265
Alexander Korsunsky <A.Korsunsky@gmail.com>
2023-05-06 00:14:54 +0200

    transforms: Add ColorJitter at augmentation lvl2

--- transforms.py
+++ transforms.py
@@ -90,6 +90,7 @@ def make_transforms(mean, std, augment_level=0):
         oplist.append(T.RandomResizedCrop(size=256, scale=(0.3, 1.0), ratio=(1,1), antialias=antialias))
         oplist.append(T.RandomHorizontalFlip())
     if augment_level >= 2:
+        oplist.append(T.ColorJitter(brightness=.3, hue=.2, contrast=.4, saturation=.3))
         oplist.append(T.GaussianBlur(kernel_size=[5, 5], sigma=2))
     
     assert augment_level <= 2, "Augmentation level "+str(augment_level)+"?? What is this, the future??"
